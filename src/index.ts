14064d79a51ef2ead0607851b16a6a6e343b4b96
lroskoshin <lev_roskoshin@protonmail.com>
2022-10-14 18:14:25 +0300

    fixes

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,39 @@
 'use strict'
 import { createCounter } from "./create-counter";
 
-function ctrlButton() {
-    submit.disabled = this.value.trim().length === 0;
-}
-cartname.addEventListener('input', ctrlButton, false);
-ctrlButton.call(cartname);
+const cart = document.getElementById("cart") as HTMLElement
+const cartname = document.getElementById('cartname') as HTMLInputElement;
 
+const submit = document.getElementById('submit') as HTMLButtonElement;
+submit.addEventListener('click', addCart);
 
-var inputEl = document.getElementById('cartname');
+cartname.addEventListener('input', (event) => {
+    if(event instanceof InputEvent && event.target instanceof HTMLInputElement) {
+        submit.disabled = event.target.value.trim().length === 0;
+    }
+}, false);
 
-document.querySelector('#cartname')?.addEventListener('keypress', function (e) {
+cartname.addEventListener('keypress', (e) => {
     if (e.key === 'Enter') {
-        addCart('cartname');
-        inputEl.value = '';
+        addCart();
+        cartname.value = '';
     }
 });
 
 
-const cart = document.getElementById("cart")
-
-const burger = createCounter("Burger");
-cart?.appendChild(burger);
-
-const cocain = createCounter("Coca-Cola");
-cart?.appendChild(cocain);
-
-document.querySelector("#submit")?.addEventListener('click', addCart);
-
 function addCart() {
-    const nameInputElement = document.querySelector("#cartname");
-    if(nameInputElement !== null && nameInputElement instanceof HTMLInputElement) {
-        const name = nameInputElement.value;
-
+    const name = cartname.value;
+    if(name.trim().length > 0) {
         const newCart = createCounter(name);
         cart?.appendChild(newCart);
-        inputEl.value = '';
-    } else {
-        console.error("Не найден инпут")
+        cartname.value = '';
     }
 }
+
+
+// Initialization
+const burger = createCounter("Burger");
+cart.appendChild(burger);
+
+const cocain = createCounter("Coca-Cola");
+cart.appendChild(cocain);
